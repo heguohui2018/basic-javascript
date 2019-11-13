fd6b48ea6d19f2b869f757a77db7eea06c91c464
heguohui2018 <heguohui2017@outlook.com>
2019-11-13 21:13:14 +0800

    commit

---
+++ learn-jquery/jquery-event.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("button").click(function () { 
+
+		// this当前对象
+		$(this).hide();
+		// .hide()方法用于隐藏对象
+	});
+})
--- learn-jquery/jquery-selector.js
+++ learn-jquery/jquery-selector.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
+	// 给按钮增加一个点击事件
 	$("button").click(function(){
+
+		// 元素选择器
 		$("p").text("p元素被修改");
+
+		// id选择器
 		$("#h2-id").text("修该h2标题");
+
+		// 类选择器
+		$(".red").text("一级标题被修改了")
 	})
 })
