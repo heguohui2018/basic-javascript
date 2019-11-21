d1e929ab3decd36b418b6a9848278a47beaaf4dc
heguohui2018 <heguohui2017@outlook.com>
2019-11-21 10:54:41 +0800

    commit

--- javascript基础/js_oop_构造函数.js
+++ javascript基础/js_oop_构造函数.js
@@ -1,5 +1,6 @@
 // 创建对象的一个一个实例，可以采用直接创建对象实例的方法
 // 但是，创建一个对象多个实例时需要构造函数,对象构造函数
+// 对象构造函数的好处是会创建模版，方便多次实例化
 function myNewObjectType() {
 	this.info = "I an a new object";
 	this.showInfo = function(){
