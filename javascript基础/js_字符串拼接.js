e18c77bec72c5be118ac749676f57d7d7f2c736e
heguohui2018 <heguohui2017@outlook.com>
2019-12-06 10:20:51 +0800

    commit

--- javascript基础/js_字符串拼接.js
+++ javascript基础/js_字符串拼接.js
@@ -11,3 +11,10 @@ console.log(num1+num2+num3+num4);
 let newString = " ".concat("This","is","a","test");
 console.log(newString);
 	
+
+let strName = prompt("What 's your name"," ");
+if (strName == "Shelley"){
+	alert("Your name is Shelley! Good for you!");
+}else{
+	alert("Your name isn't Shelley.Bummer.");
+}
