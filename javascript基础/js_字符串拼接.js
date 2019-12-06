6e158ccb4ec7e8b1737c070bc5e9052535ac3f8a
heguohui2018 <heguohui2017@outlook.com>
2019-12-06 09:40:38 +0800

    commit

---
+++ javascript基础/js_字符串拼接.js
@@ -0,0 +1,13 @@
+let num1 = "This is a ";
+let num2 = "apple";
+console.log(num1+num2);
+
+let num1 = "This"
+let num2 = "is"
+let num3 = "a"
+let num4 = "text"
+console.log(num1+num2+num3+num4);
+
+let newString = " ".concat("This","is","a","test");
+console.log(newString);
+	
