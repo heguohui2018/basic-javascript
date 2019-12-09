2761414dba805294cf05620e37964320a9931cf8
heguohui2018 <heguohui2017@outlook.com>
2019-12-09 15:06:01 +0800

    commit

--- javascript基础/js_数组.js
+++ javascript基础/js_数组.js
@@ -33,3 +33,45 @@ console.log(number);
 // 第一个参数为起始索引值，后面一个为需要删除的元素个数
 number.splice(5,3)
 console.log(number);
+
+// 从索引值为5的位置删除三个元素，同时也添加三个元素
+number.splice(5,3,2,3,4);
+console.log(number);
+
+// 数组只能存储一维数据，存储二维数据需要通过数组嵌套来实现
+// 第一天
+averageTemp[0] = [];
+averageTemp[0][0] = 72;
+averageTemp[0][1] = 73;
+averageTemp[0][2] = 74;
+averageTemp[0][3] = 75;
+averageTemp[0][4] = 76;
+averageTemp[0][5] = 77;
+averageTemp[0][6] = 78;
+averageTemp[1] = [];
+averageTemp[1][0] = 82;
+averageTemp[1][1] = 83;
+averageTemp[1][2] = 84;
+averageTemp[1][3] = 85;
+averageTemp[1][4] = 86;
+averageTemp[1][5] = 87;
+averageTemp[1][6] = 88;
+
+// 2*2矩阵
+function printMatrix(myMatrix) {
+	for(let i = 0;i<myMatrix.length;i++){
+		for(let j =0;j<myMatrix.length[i];j++){
+			console.log(myMatrix[i][j]);
+		}
+j}
+}
+printMatrix(averageTemp);
+
+
+
+
+
+
+
+
+
