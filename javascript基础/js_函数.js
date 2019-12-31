1404e14048ee2fb0230effb58f0d729390dc49e7
heguohui2018 <heguohui2017@outlook.com>
2019-12-31 09:13:47 +0800

    add 一些修改

--- javascript基础/js_函数.js
+++ javascript基础/js_函数.js
@@ -32,3 +32,43 @@ function showMessage() {
 showMessage();
 // 当内部变量与外部变量同名是，内部变量会覆盖外部变量，
 
+// from,text 是函数的两个参数
+function showMessage(from,text) {
+	alert(from+':'+text);
+}
+showMessage('Ann','hello!');
+showMessage('Ann','What\'s up?');
+// 函数调用的时候给定值会被复制到局部变量from和text参数里
+
+
+function showMessage(from,text) {
+	from = '*'+from+'*';
+	alert(from+':'+text);
+}
+
+let from = 'Ann';
+showMessage(from,"hello");
+alert(from);
+// 程序运行逻辑:首先将调用函数值拷贝到参数里,
+			 // 然后是将外部变量from传递到局部变量from中的赋值语句中
+
+// 函数调用时没有提供参数的值，函数的运行结果会出现: undefined
+function showMessage(from,text) {
+	alert(from+':'+text);
+}
+showMessage('Ann');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
