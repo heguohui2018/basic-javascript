6b9452912ba51a7ef1e38a449634b5d4a9b97d77
heguohui2018 <heguohui2017@outlook.com>
2019-12-31 17:54:40 +0800

    add 一些修改

--- javascript基础/js_函数.js
+++ javascript基础/js_函数.js
@@ -58,17 +58,54 @@ function showMessage(from,text) {
 }
 showMessage('Ann');
 
+// 给参数设定默认的值,当参数的值未给定时会调用参数的默认值
+function showMessage(from,text="no text giveen") {
+	alert(from+':'+text);
+}
+showMessage('Ann');
 
 
+function bookMessage(username,page=800) {
+	alert(username+':'+page);
+}
+bookMessage('heguohui');
 
 
+function sum(a,b) {
+	return a+b;
+}
+let result = sum(1, 2);
+alert(result);
 
 
+// 返回a,b的最小值
+function getMin(c,d) {
+	if(c<d){
+		return c;
+	}else{
+		return d;
+	}
+}
+let result = getMin(1, 3);
+alert(result);
 
 
+// 计算x的n次方
+function pow(x,n) {
+	let result = x;
+	
+	for(let i=1;i<n;i++){
+		result *= x;
+	}
+	
+	return result;
+}
 
-
-
-
-
+let x = prompt("x?","");
+let n = prompt("n?","");
+if(n<1){
+	alert('Power ${n} is not support ,use a positive integer')
+}else{
+	alert(pow(x, n));
+}
 
