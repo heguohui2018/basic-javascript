45592fb3b84a6662aee5c189d60378f646ed1318
heguohui2018 <heguohui2017@outlook.com>
2020-01-20 14:24:34 +0800

    js操作符

---
+++ javascript基础/js_操作符.js
@@ -0,0 +1,32 @@
+let num = 0;
+
+// 比较操作符
+// == 相等
+console.log('num ==1 :'+(num == 1));
+
+// === 全相等
+console.log('num === 1 :'+(num === 1));
+
+// !=不等于
+console.log('num != 1 :'+(num != 1));
+
+// > 大于
+console.log('num > 1 :'+(num > 1));
+
+// < 小于
+console.log('num < 1 :'+(num < 1));
+
+// <= 小于等于
+console.log('num <= 1 :'+(num <= 1));
+
+// >=大于等于
+console.log('num >= 1 :'+(num >= 1));
+
+// && 与
+console.log('true && false :' +(true && false));
+
+// || 或
+console.log('true || false :' +(true || false));
+
+// ! 非
+console.log('!true:' +(!true));
