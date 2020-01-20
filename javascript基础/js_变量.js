aba9b713a761be339a8a94c1e617fc07bea49df0
heguohui2018 <heguohui2017@outlook.com>
2020-01-20 12:17:37 +0800

    js变量及数据类型

---
+++ javascript基础/js_变量.js
@@ -0,0 +1,23 @@
+// 数字类型
+let num = 1;
+console.log(typeof num);
+
+// 数字类型
+let price = 1.5;
+console.log(typeof price);
+
+// 字符串类型
+let name = "heguohui";
+console.log(typeof name);
+
+// 布尔型
+let trueValue = true;
+console.log(typeof trueValue);
+
+// 对象类型
+let nullValue = null;
+console.log(typeof nullValue);
+
+// undefined类型
+let age; 
+console.log(typeof age);
